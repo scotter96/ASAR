96779000dc742bdd5296d61d1c94af7d7d34e3ed
Rizki Pratama <eyefins7896@gmail.com>
2021-02-09 23:32:48 +0700

    Initial commit

---
+++ Assets/Scripts/Catalogue.cs
@@ -0,0 +1,70 @@
+﻿using Firebase;
+using Firebase.Database;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Catalogue: MonoBehaviour {
+
+  public List<object> initialDatas;
+
+  void Awake() {
+    FirebaseApp.CheckAndFixDependenciesAsync();
+  }
+
+  void Start() {
+    string[] selectColumns = {"name"};
+    DBRead(
+      tableName: "product_item",
+      knownKey: "category_code",
+      knownValue: "1",
+      selectColumns: selectColumns
+    );
+    Debug.Log(initialDatas);
+  }
+
+  void DBRead(string tableName, string knownKey, object knownValue=null, string[] selectColumns=null) {
+    // ? Get the root reference location of the database.
+    DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+    // ? Get the reference based on self-made `table name`
+    reference.Database.GetReference(tableName).GetValueAsync().ContinueWith(task => {
+      // ? What to do if the task fails
+      if (task.IsFaulted)
+        Debug.LogError($"Failed to retrieve data from table \"{tableName}\" !");
+      // ? Fetch the data
+      else if (task.IsCompleted) {
+        IEnumerable<DataSnapshot> datas = task.Result.Children;
+        foreach (DataSnapshot data in datas) {
+          // ? This is coded accordingly to my JSON format (see `database.json` file)
+          Dictionary<string,object> tempData = data.Value as Dictionary<string,object>;
+          object val;
+          if (!tempData.TryGetValue(knownKey, out val))
+            Debug.LogError($"No such key as \"{knownKey}\" !");
+          else {
+            if (knownValue == null) {
+              initialDatas.Add(val);
+            }
+            else {
+              if (val.ToString() == knownValue.ToString()) {
+                if (selectColumns == null) {
+                  initialDatas.Add(val);
+                }
+                else {
+                  Dictionary<string,object> line = new Dictionary<string, object>();
+                  foreach (string key in selectColumns) {
+                    object neededValue;
+                    if (!tempData.TryGetValue(key, out neededValue))
+                      Debug.LogError($"No such key as \"{key}\" !");
+                    else {
+                      line.Add(key,neededValue);
+                    }
+                  }
+                  initialDatas.Add(line);
+                }
+              }
+            }
+          }
+        }
+      }
+    });
+  }
+}
---
+++ Assets/Scripts/Mockup_Menu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mockup_Menu : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().name == "Menu Alamat")
+                OpenScene("Menu Katalog");
+            else if (SceneManager.GetActiveScene().name == "Menu AR")
+                OpenScene("Menu Utama");
+            else if (SceneManager.GetActiveScene().name == "AR")
+                OpenScene("Menu AR");
+            else if (SceneManager.GetActiveScene().name == "Menu Checkout")
+                OpenScene("Menu Alamat");
+            else if (SceneManager.GetActiveScene().name == "Menu Katalog")
+                OpenScene("Menu Utama");
+            else if (SceneManager.GetActiveScene().name == "Menu Payment")
+                OpenScene("Menu Checkout");
+            else if (SceneManager.GetActiveScene().name == "Menu Product")
+                OpenScene("Menu Katalog");
+            else if (SceneManager.GetActiveScene().name == "Menu Tentang")
+                OpenScene("Menu Utama");
+            else if (SceneManager.GetActiveScene().name == "Menu Utama")
+                ExitApp();
+        }
+        
+    }
+    public void OpenScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+}
